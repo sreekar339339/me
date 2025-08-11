38e79f2aeded37211b3e1af9ffbfab6b23cc0021
sreekar339339 <158072804+sreekar339339@users.noreply.github.com>
2025-08-11 13:30:36 +0000

    fix deploy script again

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
+			fallback: process.env.BASE_PATH + '/404.html',
 			precompress: false,
 			strict: true
 		}),
